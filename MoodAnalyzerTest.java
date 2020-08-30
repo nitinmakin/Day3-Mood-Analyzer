09e8ecf6bd42a232e629e48320ee26efd34b180c
nitinmakin <nitinmakin124.com>
2020-08-30 21:45:17 +0530

    Added-Throw NullPointerException if user mood is null

--- MoodAnalyzerTest.java
+++ MoodAnalyzerTest.java
@@ -1,34 +1,54 @@
-package com.bridgelabz.moodanalyser;
-import org.hamcrest.CoreMatchers;
+package com.bridgelabz.moodanalyzer;
+import com.bridgelabz.moodanalyzer.service.MoodAnalyser;
+import com.bridgelabz.moodanalyzer.service.MoodAnalyzerException;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import sun.font.CoreMetrics;
-
+import org.junit.rules.ExpectedException;
 public class MoodAnalyserTest
 {
-     @Test
-    public void iAmIn_SadMood_ShuldReturn_sad()
+    @Test
+    public void iAmIn_SadMood_ShouldReturn_Sad() throws MoodAnalyzerException
     {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
-        String mood = moodAnalyser.analyseMood();
+        String  mood = moodAnalyser.analyseMood();
+        try
+        {
+            mood = moodAnalyser.analyseMood();
+        } catch (MoodAnalyzerException e)
+        {
+            e.printStackTrace();
+        }
         Assert.assertEquals("sad" , mood);
     }
-
-     @Test
-    public void iAmIn_anyMood_ShuldReturn_happy()
+    @Test
+    public void iAmIn_anyMood_ShouldReturn_Happy() throws MoodAnalyzerException
     {
+
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
         String mood = moodAnalyser.analyseMood();
+        try
+        {
+            mood = moodAnalyser.analyseMood();
+        } catch (MoodAnalyzerException e)
+        {
+            e.printStackTrace();
+        }
         Assert.assertEquals("happy" , mood);
     }
 
-     @Test
-    public void null_Should_Return_Happy_Mood()
-    {
+    @Test
+    public void when_Null_Should_Return_Message() throws MoodAnalyzerException {
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-        String mood = moodAnalyser.analyseMood();
-        Assert.assertEquals("happy" , mood);
+        try
+        {
+            moodAnalyser.analyseMood();
+        } catch (MoodAnalyzerException e)
+        {
+            Assert.assertEquals("please enter valid message", e.getMessage());
+            System.out.println("enter valid message");
+        }
+
+
     }
 
 }
